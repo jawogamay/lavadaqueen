51a411d0395a2a924c209d13afd7fe55b9a0a9bc
Nino Orendain <ninomasbang@gmail.com>
2019-04-26 01:57:17 -0700

    database changes

--- config/database.php
+++ config/database.php
@@ -1,5 +1,10 @@
 <?php
 
+$url = parse_url(getenv("DATABASE_URL"));
+$host = $url["host"]??null;
+$username = $url["user"]??null;
+$password = $url["pass"]??null;
+$database = substr($url["path"], 1)??null;
 return [
 
     /*
@@ -13,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql_production'),
 
     /*
     |--------------------------------------------------------------------------
@@ -59,7 +64,7 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
+        'pgsql_production' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
